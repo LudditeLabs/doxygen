219c6ed050d46aae7cea0ac1e8681873dd4bc99a
Sergey Kozlov <skozlovf@gmail.com>
2018-06-26 21:10:47 +0600

    Add dump()

--- autodoc/tests/pytests/conftest.py
+++ autodoc/tests/pytests/conftest.py
@@ -1,1 +1,15 @@
 import pytest
+import sys
+
+def dump(node, indent=0):
+    offset = ' ' * indent
+    sys.stderr.write('{}{}'.format(offset, node.shortrepr()))
+    # print(offset, node.shortrepr())
+    for c in node.children:
+        dump(c, indent + 2)
+
+
+def pytest_namespace():
+    return {'g': {
+        'dump': dump,
+    }}
