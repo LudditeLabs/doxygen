aebe653bec411e0fdd7ba42d3d24af4b78374e52
Sergey Kozlov <skozlovf@gmail.com>
2018-08-13 19:00:28 +0600

    Improve dump

--- autodoc/tests/dump.py
+++ autodoc/tests/dump.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
+import os.path as op
 import pickle
+import sqlite3
 
 
 def dump(node, indent=0):
@@ -10,6 +12,32 @@ def dump(node, indent=0):
         dump(c, indent + 2)
 
 
-if __name__ == '__main__':
-    document = pickle.load(open(sys.argv[1], 'rb'))
+def dump_docblock(filename, rowid):
+    if not op.exists(filename):
+        sys.stderr.write("[ERROR] Can't find %s\n", filename)
+        sys.exit(1)
+
+    conn = sqlite3.connect(filename)
+    res = conn.execute('SELECT doc FROM docblocks where rowid=?', (rowid,))
+    res = res.fetchone()[0]
+
+    if res is None:
+        sys.stderr.write("[ERROR] Can't find row %d\n", rowid)
+        sys.exit(1)
+
+    document = pickle.loads(res)
     dump(document)
+
+# if __name__ == '__main__':
+#     document = pickle.load(open(sys.argv[1], 'rb'))
+#     dump(document)
+
+
+if __name__ == '__main__':
+    rowid = int(sys.argv[1])
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+    else:
+        filename = '/tmp/autodocdb/content.db'
+
+    dump_docblock(filename, rowid)
